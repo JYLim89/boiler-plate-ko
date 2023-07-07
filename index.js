8308e72353821be9870f2b8dbebba0c045e5fc39
JYLim89 <ljypower1@gmail.com>
2023-07-08 08:41:00 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
+const config = require('./config/key');
+
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ljypower1:tkfkdgo1@boilerplate.znhr9ud.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongDB Connected...'))
   .catch(err => console.log(err) )
@@ -12,6 +21,28 @@ mongoose.connect('mongodb+srv://ljypower1:tkfkdgo1@boilerplate.znhr9ud.mongodb.n
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', async (req, res) => {
+  // Instantiate a new user object with the data from req.body
+  const user = new User(req.body);
+
+  // Attempt to save the new user to the database
+  await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({
+        success: false,
+        err: err
+      });
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
